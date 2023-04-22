a267d95729a7e2cb4a61e1254a1ccc7b2eefc083
Alexsilva09 <alexroquesilva2@gmail.com>
2023-04-22 17:29:51 -0300

    created rev 1 codeburger

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import {Router} from 'express'
+import { v4} from 'uuid'
+
+import User from './app/models/User'
+
+const routes = new Router()
+
+routes.get('/', async (request, response)=>{
+
+    const user = await User.create({
+        id: v4(),
+        name: "Alex",
+        email: "alexcds@live.com",
+        password: '12345',
+    })
+
+
+    return response.json(user)
+
+})
+
+export default routes
